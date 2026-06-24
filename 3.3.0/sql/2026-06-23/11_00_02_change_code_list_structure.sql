368799c7a71b6637d87b2080d78e32e1a7537411
Tony Chemit <dev@tchemit.fr>
2026-06-24 18:41:09 +0200

    Finalize updates to code list structure
    - Set `NOT NULL` constraints on multiple columns across various tables.
    - Drop unused `description_en` and `description_fr` columns from several references tables.
    - Adjust column data types for `surface_fishery_activities` and `target_species` tables.

--- 3.3.0/sql/2026-06-23/11_00_02_change_code_list_structure.sql
+++ 3.3.0/sql/2026-06-23/11_00_02_change_code_list_structure.sql
@@ -1,44 +1,6 @@
-ALTER TABLE refs_biology.bait_conditions DROP COLUMN description_fr, DROP COLUMN description_en;
-ALTER TABLE refs_biology.bait_types DROP COLUMN description_fr, DROP COLUMN description_en;
-ALTER TABLE refs_biology.depredation_sources DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_biology.measurement_tools DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_biology.sampling_methods_for_catch_estimation DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_biology.sampling_methods_for_sampling_collections DROP COLUMN description_fr, DROP COLUMN description_en;
-ALTER TABLE refs_biology.species DROP COLUMN description_fr, DROP COLUMN description_en;
 ALTER TABLE refs_biology.species RENAME COLUMN "ORDER" TO species_order;
 ALTER TABLE refs_biology.species RENAME COLUMN family TO species_family;
 ALTER TABLE refs_biology.species DROP COLUMN iucn_status_code;
-ALTER TABLE refs_biology.species_aggregates DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.bait_fishing_methods DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.branchline_storages DROP COLUMN description_fr, DROP COLUMN description_en;
-ALTER TABLE refs_fishery.catch_units DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.dehooker_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.fish_storage_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.float_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.fish_preservation_methods DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.hull_material_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.light_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.light_colours DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.line_material_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.mitigation_devices DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.net_colours DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.net_configurations DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.net_deploy_depths DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.net_setting_strategies DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.offal_management_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.pole_material_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.reasons_days_lost DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.school_detection_methods DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.school_type_categories DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.sinker_material_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.streamer_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.stunning_methods DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.transhipment_categories DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.vessel_architectures DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.vessel_sections DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.vessel_size_types DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.waste_categories DROP COLUMN description_fr, DROP COLUMN description_en;
--- ALTER TABLE refs_fishery.waste_disposal_methods DROP COLUMN description_fr, DROP COLUMN description_en;
 
 ALTER TABLE refs_admin.fleet_to_flags_and_fisheries ADD COLUMN id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
 
@@ -63,4 +25,75 @@ ALTER table refs_admin.ports DROP CONSTRAINT pk_cl_ports;
 ALTER TABLE refs_admin.ports ADD CONSTRAINT pk_refs_admin_ports PRIMARY KEY (code);
 ALTER TABLE refs_admin.ports DROP COLUMN id;
 
+-- remove any rows of this table, the reference data synchronization will recreate them
 DELETE from refs_admin.fleet_to_flags_and_fisheries;
+
+-- refs_admin.cpcs
+ALTER TABLE "refs_admin"."cpcs" DROP COLUMN "description_fr";
+ALTER TABLE "refs_admin"."cpcs" DROP COLUMN "description_en";
+
+-- refs_biology.bait_conditions
+ALTER TABLE "refs_biology"."bait_conditions" DROP COLUMN "description_fr";
+ALTER TABLE "refs_biology"."bait_conditions" DROP COLUMN "description_en";
+
+-- refs_biology.sex
+ALTER TABLE "refs_biology"."sex" DROP COLUMN "description_fr";
+ALTER TABLE "refs_biology"."sex" DROP COLUMN "description_en";
+
+-- refs_biology.species_categories
+ALTER TABLE "refs_biology"."species_categories" DROP COLUMN "description_fr";
+ALTER TABLE "refs_biology"."species_categories" DROP COLUMN "description_en";
+
+-- refs_biology.species_groups
+ALTER TABLE "refs_biology"."species_groups" DROP COLUMN "description_fr";
+ALTER TABLE "refs_biology"."species_groups" DROP COLUMN "description_en";
+
+-- refs_biology.types_of_fate
+ALTER TABLE "refs_biology"."types_of_fate" DROP COLUMN "description_fr";
+ALTER TABLE "refs_biology"."types_of_fate" DROP COLUMN "description_en";
+
+-- refs_fishery.net_conditions
+ALTER TABLE "refs_fishery"."net_conditions" DROP COLUMN "description_fr";
+ALTER TABLE "refs_fishery"."net_conditions" DROP COLUMN "description_en";
+
+-- refs_fishery.reasons_days_lost
+ALTER TABLE "refs_fishery"."reasons_days_lost" DROP COLUMN "description_fr";
+ALTER TABLE "refs_fishery"."reasons_days_lost" DROP COLUMN "description_en";
+
+-- refs_fishery.school_detection_methods
+ALTER TABLE "refs_fishery"."school_detection_methods" DROP COLUMN "description_fr";
+ALTER TABLE "refs_fishery"."school_detection_methods" DROP COLUMN "description_en";
+
+-- refs_fishery.surface_fishery_activities
+ALTER TABLE "refs_fishery"."surface_fishery_activities" ALTER COLUMN "code" TYPE character varying(3);
+ALTER TABLE "refs_fishery"."surface_fishery_activities" ALTER COLUMN "name_en" TYPE character varying(512);
+ALTER TABLE "refs_fishery"."surface_fishery_activities" ALTER COLUMN "name_fr" TYPE character varying(512);
+
+-- refs_fishery.waste_categories
+ALTER TABLE "refs_fishery"."waste_categories" DROP COLUMN "description_fr";
+ALTER TABLE "refs_fishery"."waste_categories" DROP COLUMN "description_en";
+
+-- refs_fishery.waste_disposal_methods
+ALTER TABLE "refs_fishery"."waste_disposal_methods" DROP COLUMN "description_fr";
+ALTER TABLE "refs_fishery"."waste_disposal_methods" DROP COLUMN "description_en";
+
+-- refs_fishery_config.target_species
+ALTER TABLE "refs_fishery_config"."target_species" ALTER COLUMN "description_en" TYPE character varying(600);
+ALTER TABLE "refs_fishery_config"."target_species" ALTER COLUMN "description_fr" TYPE character varying(600);
+
+-- refs_admin.ports
+ALTER TABLE "refs_admin"."ports" DROP COLUMN "description_fr";
+ALTER TABLE "refs_admin"."ports" DROP COLUMN "description_en";
+
+-- refs_biology.species
+ALTER TABLE "refs_biology"."species" DROP COLUMN "description_fr";
+ALTER TABLE "refs_biology"."species" DROP COLUMN "description_en";
+
+-- refs_gis.areas
+ALTER TABLE "refs_gis"."areas" DROP COLUMN "description_fr";
+ALTER TABLE "refs_gis"."areas" DROP COLUMN "description_en";
+
+-- refs_biology.species_aggregates
+ALTER TABLE "refs_biology"."species_aggregates" DROP COLUMN "description_fr";
+ALTER TABLE "refs_biology"."species_aggregates" DROP COLUMN "description_en";
+
